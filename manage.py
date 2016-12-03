0e06515bdd6035fd8e7ea879e4fdea0afb379b2d
Semen Efremov <s.efremov@semrush.com>
2016-12-03 23:14:45 +0300

    release analyze resource properties

--- manage.py
+++ manage.py
@@ -2,10 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import re
 import logging
 
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+RESOURCE_WOOD = 1
+RESOURCE_FOOD = 2
+RESOURCE_IRON = 3
+RESOURCE_CLAY = 4
 
 
 class Manager(object):
@@ -13,7 +19,10 @@ class Manager(object):
     TIMEOUT = 20
     HOST = 'http://ts1.travian.ru'
     is_logged = False
-    VILLAGE_RESOURCE_BUILDINGS = {}
+    VILLAGE_RESOURCE_BUILDINGS = []
+    VILLAGE_RESOURCE_PRODUCTION = {}
+    VILLAGE_BUILD_QUEUE_SLOTS = 0
+    VILLAGE_BUILD_QUEUE_SLOTS_LIMIT = 1
 
     def __init__(self, user, passwd):
         self.user = user
@@ -24,9 +33,12 @@ class Manager(object):
         self.driver = webdriver.Firefox(capabilities=firefox_capabilities, timeout=self.TIMEOUT)
         self.driver.implicitly_wait(self.TIMEOUT)
 
+    def close(self):
+        if self.driver:
+            self.driver.quit()
+
     def run(self):
         self.login()
-
         if not self.is_logged:
             logging.error('login error')
             return
@@ -34,6 +46,8 @@ class Manager(object):
         while True:
             # анализируем деревню
             self.analyze()
+
+            # есть пара зданий на улучшение
             # если есть свободные ресурсы и место в очереди
                 # развиваем деревню (ресурсы и центр)
 
@@ -41,7 +55,8 @@ class Manager(object):
             # todo отправляем героя в приключения
             # todo прокачиваем героя
             # todo выполняем задания и забираем награды за них
-            pass
+            # todo sleep
+            return
 
     def login(self):
         logging.info('login call')
@@ -57,10 +72,80 @@ class Manager(object):
             logging.info('login success %s' % village_map_element)
         except:
             logging.info('login not success')
-
+        
     def analyze(self):
         logging.info('analyze call')
 
+        # analyze resource buildings
+        self._analyze_resource_buildings()
+        
+        # analyze resource input
+        self._analyze_resource_production()
+
+        # analyze buildings queue
+        self._analyze_buildings_queue()
+
+    def _analyze_resource_buildings(self):
+        self.driver.get(self.HOST + '/dorf1.php')
+        map_content = self.driver.find_element_by_id('rx')
+        builds = map_content.find_elements_by_tag_name('area')
+        result_builds = []
+        for b in builds:
+            b_desc = b.get_attribute('alt')
+            logging.debug('analyze resource build %s', b_desc)
+            if not 'Уровень' in b_desc:
+                continue
+
+            b_id = re.findall(r'id=(\d+)', str(b.get_attribute('href')))[0]
+            b_type = None
+            if 'Лесопилка' in b_desc:
+                b_type = RESOURCE_WOOD
+            elif 'Ферма' in b_desc:
+                b_type = RESOURCE_FOOD
+            elif 'Железный' in b_desc:
+                b_type = RESOURCE_IRON
+            elif 'Глиняный' in b_desc:
+                b_type = RESOURCE_CLAY
+
+            result_builds.append({
+                'id': b_id,
+                'desc': b_desc,
+                'type': b_type
+            })
+        logging.info('found %d resource buildings', len(result_builds))
+        logging.debug(result_builds)
+        self.VILLAGE_RESOURCE_BUILDINGS = result_builds
+
+    def _analyze_resource_production(self):
+        self.driver.get(self.HOST + '/dorf1.php')
+        table = self.driver.find_element_by_id('production')
+        result_production = {}
+        for row in table.find_elements_by_tag_name('tr')[1:]:
+            title = str(row.find_element_by_class_name('res').text)
+            value = str(row.find_element_by_class_name('num').text).strip()
+            value = int(re.findall(r'(\d+)', value)[0])
+            logging.debug('production %s %d', title, value)
+            type = None
+            if 'Глина' in title:
+                type = RESOURCE_CLAY
+            elif 'Зерно' in title:
+                type = RESOURCE_FOOD
+            elif 'Древесина' in title:
+                type = RESOURCE_WOOD
+            elif 'Железо' in title:
+                type = RESOURCE_IRON
+            result_production[type] = value
+        logging.info('resource production %s', result_production)
+        self.VILLAGE_RESOURCE_PRODUCTION = result_production
+
+    def _analyze_buildings_queue(self):
+        cnt = 0
+        self.driver.get(self.HOST + '/dorf1.php')
+        div = self.driver.find_element_by_class_name('buildingList')
+        if div:
+            cnt = len(div.find_elements_by_class_name('buildDuration'))
+        self.VILLAGE_BUILD_QUEUE_SLOTS = max(0, self.VILLAGE_BUILD_QUEUE_SLOTS_LIMIT - cnt)
+        logging.info('build queue free slots %d', self.VILLAGE_BUILD_QUEUE_SLOTS)
 
 
 if __name__ == '__main__':
@@ -73,6 +158,11 @@ if __name__ == '__main__':
     else:
         logging.info('run %s %s' % (username, passwd))
         m = Manager(username, passwd)
-        m.run()
+        try:
+            m.run()
+        except Exception as e:
+            logging.error('exception %s', e)
+        finally:
+            m.close()
 
 
